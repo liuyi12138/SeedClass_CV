f061873bbfa575da1759b32a4337eb723d17f228
QGrain <zhiyuzhang1999@163.com>
2020-02-18 19:20:56 +0800

    add functions for loadOne and loadAll

--- week1/dataProcess.py
+++ week1/dataProcess.py
@@ -7,6 +7,31 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+def loadOne(filename):
+    # load single batch of cifar
+    datadict = unpickle(filename)
+    X = datadict[b'data']
+    Y = datadict[b'labels']
+    X = X.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype("float") # reshape to (10000, 32, 32, 3)
+    Y = np.array(Y)
+    return X, Y
+
+def loadAll(path):
+    prefix = 'data_batch_'
+    valid_idx = 5
+    for surfix in range(1, 7):
+        if surfix == 1:
+            x_train, y_train = loadOne(path + '/' + prefix + str(surfix))
+        elif surfix == valid_idx:
+            x_valid, y_valid = loadOne(path + '/' + prefix + str(surfix))
+        else:
+            X, Y = loadOne(path + '/' + prefix + str(surfix))
+            x_all = np.concatenate((x_all, X), axis = 0)
+            y_all = np.concatenate((y_all, Y), axis = 0)
+    del X, Y
+    x_test, y_test = loadOne(path + '/test_batch')
+    return x_train, y_train, x_valid, y_valid, x_test, y_test
+
 def loadData(filename):
     dataDict = unpickle(filename)
     data = dataDict[ b'data']
@@ -28,8 +53,9 @@ def plotSample():
             if i == 0:
                 plt.title(classname)
 
-x, y = loadData("D:\HUST\寒假课程资料\数字图像处理\课设\week1\cifar-10-batches-py\data_batch_1")
-print(x.shape)
-print(y.shape)
-plotSample()
+if __name__ == '__main__':
+    x, y = loadData("D:\HUST\寒假课程资料\数字图像处理\课设\week1\cifar-10-batches-py\data_batch_1")
+    print(x.shape)
+    print(y.shape)
+    plotSample()
 
