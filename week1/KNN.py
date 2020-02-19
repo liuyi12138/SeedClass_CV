d60d124f510de1af4dd69390ded7ea88f4e08565
QGrain <zhiyuzhang1999@163.com>
2020-02-19 16:36:26 +0800

    add method evaluate to Class KNearestNeighbor

--- week1/KNN.py
+++ week1/KNN.py
@@ -3,16 +3,16 @@ from dataProcess import loadAll
 from configTemplate import dataDir
 from math import pow
 
-def LpNorm(x1,x2,p):
+def LmNorm(x1,x2,m):
     # x1 should be the xtr matrix, ndarray
     # x2 should be a row of the xpred matrix, list
     # return: dis should also be a ndarray
     dis = []
     for i in range(x1.shape[0]):
         abs_list = np.abs(x1[i] - x2)
-        psum = np.sum(abs_list**p)          # pow p and sum
-        # print(pow(psum, 1/p))
-        dis.append(pow(psum, 1/p))          # pow 1/p
+        msum = np.sum(abs_list**m)          # pow m and sum
+        # print(pow(psum, 1/m))
+        dis.append(pow(msum, 1/m))          # pow 1/m
     #print(dis)
     return np.array(dis)
 
@@ -39,16 +39,20 @@ class KNearestNeighbor:
     def __init__(self):
         pass
     
-    def train(self,x,y):
+    def train(self, x, y):
         self.xtr = x
         self.ytr = y
         
-    def predict(self,x,k):
+    def predict(self, x, k = None, m = None):
+        if k == None:
+            k = 10
+        if m == None:
+            m = 1
         print('Start to predict')
         num_test = x.shape[0]
         Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
         for i in range(num_test):
-            distances = LpNorm(self.xtr, x[i], 1)
+            distances = LmNorm(self.xtr, x[i], m)
             #distances = np.sum(np.abs(self.xtr - x[i]), axis=1)
             indexs = np.argsort(distances) #对index排序
             closestK = self.ytr[indexs[:k]] #取距离最小的K个点
@@ -57,6 +61,12 @@ class KNearestNeighbor:
             if (i+1) % 2 == 0:
                 print('now: %d/%d, Ypred[%d] = %d\r' % (i+1, num_test, i, Ypred[i]))
         return Ypred
+    
+    def evaluate(self, Ypred, y):
+        num_test = len(y)
+        num_correct = np.sum(Ypred == y)
+        accuracy = float(num_correct) / num_test
+        print('%d / %d correct => accuracy: %.3f' %(num_correct, num_test, accuracy*100)) 
 
 if __name__ == "__main__":
     x_train, y_train, x_valid, y_valid, x_test, y_test = loadAll(dataDir)
@@ -66,8 +76,5 @@ if __name__ == "__main__":
     #5-NN
     classifier = KNearestNeighbor()
     classifier.train(x_train,y_train)
-    result = classifier.predict(x_test,5)
-    num_test= len(y_test)
-    num_correct = np.sum(result == y_test)
-    accuracy = float(num_correct) / num_test
-    print('%d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+    result = classifier.predict(x_test,5,1)
+    classifier.evaluate(result, y_test)
