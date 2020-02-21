6bf63e7030c6613a69965241af81cb09d9a6bd66
QGrain <zhiyuzhang1999@163.com>
2020-02-21 20:58:31 +0800

    merge some function from master

--- week1/dataProcess.py
+++ week1/dataProcess.py
@@ -1,6 +1,9 @@
 #!/bin/env python3
 import numpy as np
 import os
+from sklearn.decomposition import PCA
+# from skimage.feature import hog
+import matplotlib.pyplot as plt
 
 def unpickle(file):
     import pickle
@@ -37,3 +40,48 @@ def loadAll(path, valid_idx = None):
     x_test, y_test = loadOne(path + '/test_batch')
     return x_train, y_train, x_valid, y_valid, x_test, y_test
 
+def pca(x_train, x_valid, n_components = None): 
+    if n_components == None:
+        n_components = 30
+
+    dir_name = './PCA'
+    xtr_pca_name = '/xtr_n_' + str(n_components) + '.npy'
+    xva_pca_name = '/xva_n_' + str(n_components) + '.npy'
+
+    # verify dir
+    if not os.path.exists(dir_name):
+        print("'./PCA' doesn't exist, create one")
+        os.makedirs(dir_name)
+    else:
+        print("'./PCA' detected, go on")
+
+
+
+    if os.path.exists(dir_name + xtr_pca_name):
+        print('PCA data detected, loading...')
+        x_train_new = np.load(dir_name + xtr_pca_name)
+        x_valid_new = np.load(dir_name + xva_pca_name)
+    else:
+        print("PCA data doesn't exist, creating...")
+        pca=PCA(n_components = n_components)
+        pca.fit(x_train)
+        x_train_new = pca.transform(x_train)
+        x_valid_new = pca.transform(x_valid)  
+        np.save(dir_name + xtr_pca_name, x_train_new)
+        np.save(dir_name + xva_pca_name, x_valid_new)
+    print('Finish PCA\n')
+    return x_train_new,x_valid_new
+
+def plotK(dataK):
+    data = []
+    for i in range(len(dataK)):
+        data.append(float(format(dataK[i],'.3f')))
+    x = range(1,len(data) + 1)
+    max_indx = np.argmax(data)
+    show_max='['+str(max_indx+1)+' '+str(data[max_indx])+']'
+    plt.plot(x, dataK, color='red', label='Hog&L1')
+    plt.legend() # 显示图例
+    plt.xlabel('K')
+    plt.ylabel('accurity')
+    plt.annotate(show_max,xytext=(max_indx,data[max_indx]),xy=(max_indx,data[max_indx]))
+    plt.show()
