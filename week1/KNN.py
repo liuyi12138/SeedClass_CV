7da504c503cb43f646d9ae0eb6d2fbefa8c70121
Leegenux <limian22@gmail.com>
2020-02-21 20:56:55 +0800

    merge zzy

--- week1/KNN.py
+++ week1/KNN.py
@@ -44,6 +44,96 @@ class NearestNeighbor:
                 print('now: %d/%d' % (i+1, num_test))
         return Ypred
 
+
+def getDistances(x1, x2, valid_idx=None, weights=None, data_type=None, value=None):
+    # Parameters Explaination:
+    #   @x1, x2:        numpy 2D-matrixs, x1 is train, x2 is valid
+    #   @valid_idx:     valid index from 1-5
+    #   @weights:       A list indicating the weights distribution of cosine, L1 and L2 distance
+    #   @data_type:     1-5, 1 for raw, 2 for Sample Grey, 3 for PCA, 4 for HOG, 5 for GreyHOG
+
+    dir_list = ['./Dis-Raw', './Dis-SampleGrey', './Dis-PCA', './Dis-HOG', './Dis-GreyHOG']
+    dir_name = dir_list[data_type - 1]
+    total_dis = '/distances.npy'  # total_dis = weights_matrix x distances_matrix
+    [cos_dis, l1_dis, l2_dis] = ['/cos_dis_' + str(value) + '.npy', '/l1_dis_' + str(value) + '.npy',
+                                 '/l2_dis_' + str(value) + '.npy']
+    weights_file = '/weights.npy'
+
+    distances_matrix = []
+    num_test = x2.shape[0]
+
+    # npy file path of cos, l1 and l2
+    cos_path = dir_name + cos_dis
+    l1_path = dir_name + l1_dis
+    l2_path = dir_name + l2_dis
+    total_path = dir_name + total_dis
+
+    # verify dir
+    if not os.path.exists(dir_name):
+        print("'./Distances' doesn't exist, create one")
+        os.makedirs(dir_name)
+    else:
+        print("'./Distances' detected, go on")
+
+    if os.path.exists(dir_name + weights_file) and os.path.exists(total_path):
+        last_weights = np.load(dir_name + weights_file)
+        if (last_weights == np.array(weights)).all() == True:
+            return np.load(total_path)
+        else:
+            print('weights matrix has been changed, recalculate total distances')
+    else:
+        print("weights or total_dis npy file doesn't exist, create one")
+    np.save(dir_name + weights_file, np.array(weights))
+
+    # get cos, L1 and L2
+    if os.path.exists(cos_path) and os.path.exists(l1_path) and os.path.exists(l2_path):
+        print('%s detected, load npy data' % cos_path)
+        print('%s detected, load npy data' % l1_path)
+        print('%s detected, load npy data' % l2_path)
+        cos_distances = np.load(cos_path)
+        l1_distances = np.load(l1_path)
+        l2_distances = np.load(l2_path)
+    else:
+        print('Start to create npy files for cosine, L1 and L2')
+        cos_distances = []
+        l1_distances = []
+        l2_distances = []
+        for i in range(num_test):
+            if (i + 1) % 100 == 0:
+                print('%d of %d finished' % ((i + 1), num_test))
+            cos_distances.append(cosDis(x1, x2[i]))
+            l1_distances.append(LmNorm(x1, x2[i], 1))
+            l2_distances.append(LmNorm(x1, x2[i], 2))
+        np.save(cos_path, np.array(cos_distances))
+        print('cosine finished')
+        np.save(l1_path, np.array(l1_distances))
+        print('L1 finished')
+        np.save(l2_path, np.array(l2_distances))
+        print('L2 finished')
+
+    # get total distances
+    print('Start total distances')
+    for i in range(num_test):
+        distances_matrix.append(
+            weights[0] * cos_distances[i] + weights[1] * l1_distances[i] + weights[2] * l2_distances[i])
+    np.save(total_path, np.array(distances_matrix))
+    print('Finish total distances')
+    return np.array(distances_matrix)
+
+
+class Optimizer:
+    def __init__(self):
+        self.dict = {'Raw': 1, 'SampleGrey': 2, 'PCA': 3, 'HOG': 4, 'GreyHOG': 5}
+        self.weights = [1, 0, 0]
+
+    def generate(self, opt_type=None, opt_value=None):
+        self.opt_type = self.dict[opt_type]
+        self.opt_value = opt_value
+
+    def setWeights(self, weights):
+        self.weights = weights
+
+
 class KNearestNeighbor:
     def __init__(self):
         return None
