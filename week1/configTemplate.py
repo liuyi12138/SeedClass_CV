e4fdd4113b6eea021c647724f0d74be077fd9d35
Leegenux <limian22@gmail.com>
2020-02-21 19:58:28 +0800

    update config

--- week1/configTemplate.py
+++ week1/configTemplate.py
@@ -1,4 +1,7 @@
 #!/bin/env python3
+import os
+
+projectPath = os.path.dirname(os.path.realpath(__file__))
 dataDir = "D:\HUST\寒假课程资料\数字图像处理\课设\week1\cifar-10-batches-py\data_batch_1"
 batchBasePath = "D:\HUST\寒假课程资料\数字图像处理\课设\week1\cifar-10-batches-py\data_batch_"
 testDataDir = ""
