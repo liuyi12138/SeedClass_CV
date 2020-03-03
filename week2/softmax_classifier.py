c6bfcef32cea36a1d4f30610f2072646e807117d
Leegenux <limian22@gmail.com>
2020-03-03 19:31:14 +0800

    Add option for biases

--- week2/softmax_classifier.py
+++ week2/softmax_classifier.py
@@ -3,8 +3,9 @@
 import numpy as np
 
 class softmax_classifier(object):
-    def __init__(self, net_layers):
+    def __init__(self, net_layers, use_biases=False):
         self._is_properly_init = True
+        self._use_biases = use_biases
         # set up the weights and biases
         if len(net_layers) <= 2:
             print("Wrong network description!")
@@ -14,11 +15,13 @@ class softmax_classifier(object):
             self._input = None
             self._output = None
             self._net_weights = []
+            if self._use_biases: self._net_biases = []
 
             weights_num = len(net_layers) - 1
             for i in range(1, weights_num):
                 self._net_weights.append(np.zeros(net_layers[i-1]), np.zeros(net_layers[i]))
-                self._net_weights.append(np.zeros(net_layers[i]))
+                if self._use_biases: self._net_biases.append(np.zeros(net_layers[i]))
+
         # set up the partial derivatives
 
 
