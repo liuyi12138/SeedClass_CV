29de9f3e5baf1dc1f7b20ea921537fb2d0e5fa67
Leegenux <limian22@gmail.com>
2020-03-06 16:27:21 +0800

    to be tested

--- week2/source/softmax_classifier.py
+++ week2/source/softmax_classifier.py
@@ -3,9 +3,8 @@
 import numpy as np
 
 class softmax_classifier(object):
-    def __init__(self, net_layer_shapes, use_biases=False):
+    def __init__(self, net_layer_shapes):
         self._is_properly_init = True
-        self._use_biases = use_biases
         # set up the weights and biases
         if len(net_layer_shapes) <= 2:
             print("Wrong network description!")
@@ -15,16 +14,13 @@ class softmax_classifier(object):
             self._input_shape = net_layer_shapes[0]
             self._output_shape = net_layer_shapes[-1]
             self._net_weights = []
-            if self._use_biases: self._net_biases = []
+            self._pending_weights = []      # used to store those to-be-applied weights
 
-            weights_num = len(net_layer_shapes) - 1
-            for i in range(1, weights_num):
+            weights_num = len(net_layer_shapes)-1
+            for i in range(1, weights_num+1):
                 weight_range = np.sqrt(6 / (net_layer_shapes[i - 1] + net_layer_shapes[i]))
-                self._net_weights.append(np.random.uniform(-weight_range, weight_range, (net_layer_shapes[i - 1], net_layer_shapes[i])))  # Xavier initialization reference https://zhuanlan.zhihu.com/p/76602243
-
-                if self._use_biases:
-                    bias_range = np.sqrt(1 / net_layer_shapes[i])
-                    self._net_biases.append(np.random.uniform(-bias_range, bias_range, net_layer_shapes[i]))
+                self._net_weights.append(np.random.uniform(-weight_range, weight_range, (net_layer_shapes[i-1]+1, net_layer_shapes[i])))  # Xavier initialization reference https://zhuanlan.zhihu.com/p/76602243
+                self._pending_weights.append(np.zeros((net_layer_shapes[i-1]+1, net_layer_shapes[i])))
 
         # set up the partial derivatives
     def _back_propagate(self, input, tag):
@@ -36,49 +32,39 @@ class softmax_classifier(object):
         if self._is_properly_init:
             # check shape
             if self._input_shape == len(input):
-                prob_results, _index, _results = self.predict(input, is_return_inter_values=True)
+                prob_results, pred_index, inter_results = self.predict(input, is_return_inter_values=True)
 
                 # output layer partial derivatives
-                output_layer_partial_derivs = np.zeros(self._output_shape)
-                for i in len(output_layer_partial_derivs):
-                    output_layer_partial_derivs[i] = prob_results[i]-1 if i == tag else prob_results[i]
+                one_hot_result = np.zeros(self._output_shape)
+                one_hot_result[tag] = 1
 
-                # todo: weights and biases back propagation
-                pass
+                # w += (x.T).dot(p-one_hot)
+                self._pending_weights[0] += np.mat(np.concatenate((input,[1]), axis=0)).T.dot(np.mat(prob_results-one_hot_result))
 
-                # store the propations result
-                pass
-
-    def _apply_propagation(self):
+    def _apply_propagation(self, division):
         """
         apply the temporary parameters updates to the weights and biases
         """
         if self._is_properly_init:
             # update all the propagation
-            pass
+            for i in len(self._net_weights):
+                self._net_weights[i] += self._pending_weights[i]/division
+                self._pending_weights *= 0      # reset the weights to zeros
 
-    def predict(self, input, is_return_inter_values=True):
+    def predict(self, input, is_return_inter_values=False):
         if self._is_properly_init:
-            inter_value = input
+            inter_value = np.concatenate((input, [1]), axis=0)
             inter_results = []
 
-            if self._use_biases:
-                for idx, weight_mat in enumerate(self._net_weights):
-                    inter_value = inter_value.dot(weight_mat)+self._use_biases[idx]
-                    if is_return_inter_values:
-                        inter_results.append(inter_value.copy())
-            else:
-                for weight_mat in self._net_weights:
-                    inter_value = inter_value.dot(weight_mat)
-                    if is_return_inter_values:
-                        inter_results.append(inter_value.copy())
+            for weight_mat in self._net_weights:
+                inter_value = inter_value.dot(weight_mat)
+                if is_return_inter_values: inter_results.append(inter_value)
 
             # softmax function result and return
             inter_value = np.exp(inter_value)
             prob_results = inter_value / np.sum(inter_value)
             pred_index = np.argmax(prob_results)
             return prob_results, pred_index, inter_results
-
 
     def batch_train(self, batch_data, tags):
         """
