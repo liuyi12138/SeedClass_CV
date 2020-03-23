0c18dfbd05218049bfd96ca185697fc28bfa4144
QGrain <zhiyuzhang1999@163.com>
2020-03-23 10:19:56 +0800

    add parameter search

--- week3-4/source/zzy.py
+++ week3-4/source/zzy.py
@@ -77,16 +77,14 @@ class softmax_classifier(object):
                 current_derivative = np.mat(prob_results - one_hot_tag)
                 for i in range(-1, -len(self._net_weights), -1):  # totally len(self._net_weights)-1
                     self._pending_weights[i] -= np.mat(np.concatenate((inter_results[i - 1], [1]))).T.dot(
-                        current_derivative
-                    ) * learning_rate
+                        current_derivative) * learning_rate
                     # derivative of results of activation function, so the biases is ignored here
                     current_derivative = current_derivative.dot(
                         self._net_weights[i][:-1, :].T)
                     # derivative of activation function, given function results
                     current_derivative = np.multiply(act_derivative(np.array(inter_results[i - 1])), current_derivative)
 
-                self._pending_weights[0] -= np.mat(np.concatenate((input, [1]))).T.dot(
-                    current_derivative) * learning_rate
+                self._pending_weights[0] -= np.mat(np.concatenate((input, [1]))).T.dot(current_derivative) * learning_rate
 
                 # next
                 return loss, net_loss
@@ -138,8 +136,8 @@ class softmax_classifier(object):
                 if idx != len(self._net_weights) - 1:
                     inter_value = act_func(inter_value)
 
-                if is_return_inter_values: inter_results.append(
-                    inter_value)  # the inter_values are results of activation function
+                if is_return_inter_values: 
+                    inter_results.append(inter_value)  # the inter_values are results of activation function
 
             # softmax function result and return
             inter_value = np.exp(inter_value)
@@ -200,111 +198,128 @@ x_testt,y_test = loadOne("../../../cifar-10-batches-py/test_batch")
 x_train = normalization(x_traint)
 x_test = normalization(x_testt)
 
+shape_list = [(3072,16,10), (3072,32,10), (3072,64,10), (3072,16,16,10), (3072,64,16,10), (3072,32,32,10)]
+lr_list = [0.03, 0.05, 0.07]
+bat_list = [16, 32, 64, 128]
+init_list = ["Xavier", "He"]
 
+cnt = 0
+for net_layer_shapes in shape_list:
+    for lr in lr_list:
+        for batch_size in bat_list:
+                cnt += 1
+                learning_rate = lr
+                print("The %d test start:\n" %cnt)
+                #开始训练
+                norm_method = 0
+                norm_ratio = 0
+                if norm_method == 1:
+                    norm_ratio = 0.0002
+                elif norm_method == 2:
+                    norm_ratio = 0.0001
 
-#开始训练
-norm_method = 0
-norm_ratio = 0
-if norm_method == 1:
-    norm_ratio = 0.0002
-elif norm_method == 2:
-    norm_ratio = 0.0001
+                # net_layer_shapes = (3072, 32, 10)
+                # batch_size = 16
+                # learning_rate = 0.05
+                learning_rate_decay = 0.933
+                epoch = 60
+                activation = "relu"
+                parameter_initializer = "Xavier"
+                optimizer = "BGD"               # GD, BGD, SGD, Momentum, AdaGrad, Adam
 
-net_layer_shapes = (3072, 32, 10)
-batch_size = 16
-learning_rate = 0.05
-learning_rate_decay = 0.98
-epoch = 100
-activation = "relu"
-parameter_initializer = "He"
-optimizer = "BGD"               # GD, BGD, SGD, Momentum, AdaGrad, Adam
+                print("net_shape = ", net_layer_shapes, "batch_size = %d, epoch = %d\nnorm_method = %d, norm_ratio = %.5f\nlearning_rate = %.3f, learning_decay = %.3f\nactivation = %s, para_initializer = %s, optimizer = %s\n" 
+                                    %(batch_size, epoch, norm_method, norm_ratio, learning_rate, learning_rate_decay, activation, parameter_initializer, optimizer))
 
-print("batch_size = %d, epoch = %d\nnorm_method = %d, norm_ratio = %.5f\nlearning_rate = %.3f, learning_decay = %.3f\nactivation = %s, para_initializer = %s, optimizer = %s\n" 
-                    %(batch_size, epoch, norm_method, norm_ratio, learning_rate, learning_rate_decay, activation, parameter_initializer, optimizer))
+                fp = open("../results/log.txt", "a+")
+                fp.write("The %d test\n" %cnt)
+                fp.write("net_shape = (%d %d %d)" %(net_layer_shapes[0], net_layer_shapes[1], net_layer_shapes[2]))
+                fp.write(", batch_size = %d, epoch = %d\nnorm_method = %d, norm_ratio = %.5f\nlearning_rate = %.3f, learning_decay = %.3f\nactivation = %s, para_initializer = %s, optimizer = %s\n" 
+                                    %(batch_size, epoch, norm_method, norm_ratio, learning_rate, learning_rate_decay, activation, parameter_initializer, optimizer))
 
-clsfir = softmax_classifier(net_layer_shapes = net_layer_shapes,
-                            norm_ratio = norm_ratio,
-                            norm_method = norm_method, 
-                            activation = activation, 
-                            parameter_initializer = parameter_initializer,
-                            optimizer = optimizer)
-loss = []
-acc = []
+                clsfir = softmax_classifier(net_layer_shapes = net_layer_shapes,
+                                            norm_ratio = norm_ratio,
+                                            norm_method = norm_method, 
+                                            activation = activation, 
+                                            parameter_initializer = parameter_initializer,
+                                            optimizer = optimizer)
+                loss = []
+                acc = []
 
-for i in range(1, epoch+1):
-    time_start = time.time()
-    n = 0
-    loss_all = 0
-    loss_net = 0
-    while (n + batch_size < len(x_train)):
-        x_temp = x_train[n: n + batch_size]
-        y_temp = y_train[n: n + batch_size]
+                for i in range(1, epoch+1):
+                    time_start = time.time()
+                    n = 0
+                    loss_all = 0
+                    loss_net = 0
+                    while (n + batch_size < len(x_train)):
+                        x_temp = x_train[n: n + batch_size]
+                        y_temp = y_train[n: n + batch_size]
 
-        loss_all_tmp, loss_net_tmp = clsfir.batch_train(x_temp, y_temp, learning_rate)
-        loss_all += loss_all_tmp
-        loss_net += loss_net_tmp
+                        loss_all_tmp, loss_net_tmp = clsfir.batch_train(x_temp, y_temp, learning_rate)
+                        loss_all += loss_all_tmp
+                        loss_net += loss_net_tmp
 
-        n += batch_size
-    loss_present = float(loss_all) / ((n / batch_size) + 1)
-    loss_net_present = float(loss_net) / ((n / batch_size) + 1)
-    loss.append(loss_present)
-    result = []
-    for j in range(10000):
-        predict = clsfir.predict(x_test[j])[1]
-        result.append(predict)
+                        n += batch_size
+                    loss_present = float(loss_all) / ((n / batch_size) + 1)
+                    loss_net_present = float(loss_net) / ((n / batch_size) + 1)
+                    loss.append(loss_present)
+                    result = []
+                    for j in range(10000):
+                        predict = clsfir.predict(x_test[j])[1]
+                        result.append(predict)
 
-    num_test= 10000
-    num_correct = np.sum(result == y_test) #计算准确率
-    accuracy = float(num_correct) / num_test
-    acc.append(accuracy)
+                    num_test= 10000
+                    num_correct = np.sum(result == y_test) #计算准确率
+                    accuracy = float(num_correct) / num_test
+                    acc.append(accuracy)
 
-    permutation = np.random.permutation(y_train.shape[0])
-    x_train = x_train[permutation]
-    y_train = y_train[permutation]
-    learning_rate *= learning_rate_decay
+                    permutation = np.random.permutation(y_train.shape[0])
+                    x_train = x_train[permutation]
+                    y_train = y_train[permutation]
+                    learning_rate *= learning_rate_decay
 
-    time_end = time.time()
-    
-    print("for epoch %d, loss is %.4f, net loss is %.4f, acc is %.4f, time cost %.2fs" %(i, loss_present, loss_net_present, accuracy, time_end-time_start))
-    
-#     plt.plot(loss)
-#     plt.show()
-    
-plt.subplot(1,2,1)
-plt.figure(figsize = (6, 4))
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.plot(range(1, epoch+1), loss)
+                    time_end = time.time()
+                    
+                    print("for epoch %d, loss is %.4f, net loss is %.4f, acc is %.4f, time cost %.2fs" %(i, loss_present, loss_net_present, accuracy, time_end-time_start))
+                    fp.write("for epoch %d, loss is %.4f, net loss is %.4f, acc is %.4f, time cost %.2fs\n" %(i, loss_present, loss_net_present, accuracy, time_end-time_start))
+                #     plt.plot(loss)
+                #     plt.show()
+                    
+                plt.figure(figsize = (6, 4))
+                plt.xlabel('Epoch')
+                plt.ylabel('Loss')
+                plt.plot(range(1, epoch+1), loss)
+                loss_figname = "../results/loss_test" + str(cnt) + ".png"
+                plt.savefig(loss_figname)
+                plt.close()
 
-plt.subplot(1,2,2)
-plt.figure(figsize = (6, 4))
-plt.xlabel('Epoch')
-plt.ylabel('Acc')
-plt.plot(range(1, epoch+1), acc)
+                plt.figure(figsize = (6, 4))
+                plt.xlabel('Epoch')
+                plt.ylabel('Acc')
+                plt.plot(range(1, epoch+1), acc)
+                acc_figname = "../results/acc_test" + str(cnt) + ".png"
+                plt.savefig(acc_figname)
+                plt.close()
 
-figname = "../results/test3-remove5.png"
-plt.savefig(figname)
-plt.show()
+                #训练集
+                result = []
+                for i in range(50000):
+                    predict = clsfir.predict(x_train[i])[1]
+                    result.append(predict)
+                    
+                num_test= 50000
+                num_correct = np.sum(result == y_train) #计算准确率
+                train_accuracy = float(num_correct) / num_test
+                print("train acc: %f" % train_accuracy)
 
-
-#训练集
-result = []
-for i in range(50000):
-    predict = clsfir.predict(x_train[i])[1]
-    result.append(predict)
-    
-num_test= 50000
-num_correct = np.sum(result == y_train) #计算准确率
-accuracy = float(num_correct) / num_test
-print("train acc: %f" % accuracy)
-
-#测试集
-result = []
-for i in range(10000):
-    predict = clsfir.predict(x_test[i])[1]
-    result.append(predict)
-    
-num_test= 10000
-num_correct = np.sum(result == y_test) #计算准确率
-accuracy = float(num_correct) / num_test
-print("test acc: %f" % accuracy)
+                #测试集
+                result = []
+                for i in range(10000):
+                    predict = clsfir.predict(x_test[i])[1]
+                    result.append(predict)
+                    
+                num_test= 10000
+                num_correct = np.sum(result == y_test) #计算准确率
+                test_accuracy = float(num_correct) / num_test
+                print("test acc: %f" % test_accuracy)
+                fp.write("train acc: %f, test acc: %f\n\n" %(train_accuracy, test_accuracy))
+                fp.close()
