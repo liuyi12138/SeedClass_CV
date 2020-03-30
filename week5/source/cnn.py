d705eba8d99cad734af98f8277b80188b14200a5
liuyi12138 <852833062@qq.com>
2020-03-30 18:22:39 +0800

    2020/3/30

--- week5/source/cnn.py
+++ week5/source/cnn.py
@@ -30,14 +30,6 @@ def normalizationImage(data):
     data = data - 0.5
     return data
 
-def oneHotY(data):
-    one_hot_data = []
-    for i in range(len(data)):
-        one_hot_temp = np.zeros(10)
-        one_hot_temp[data[i]] = 1
-        one_hot_data.append(one_hot_temp)
-    one_hot_data = np.array(one_hot_data)
-    return one_hot_data
 
 for i in range(1, 6):
     if i == 1:
@@ -48,33 +40,73 @@ for i in range(1, 6):
         y_train = np.concatenate((y_train, y_temp), axis=0)
 x_test, y_test = loadOne("../../../cifar-10-batches-py/test_batch")
 
-x_train = normalizationImage(x_train)
-x_test = normalizationImage(x_test)
+x_train = x_train.reshape(x_train.shape[0], 3, 32, 32).transpose(0, 2, 3, 1).astype("float")
+x_test = x_test.reshape(x_test.shape[0], 3, 32, 32).transpose(0, 2, 3, 1).astype("float")
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
 
-y_train = oneHotY(y_train)
-y_test = oneHotY(y_test)
+#数据生成器
+train_datagen = keras.preprocessing.image.ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=20,
+    width_shift_range=0.16,
+    height_shift_range=0.16,
+    zoom_range=0.2,
+    rescale=1./255,
+    horizontal_flip=True)
+
+test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
 
 # 网络模型参考 https://geektutu.com/post/tf2doc-cnn-cifar10.html
-# Conv2D 网络层 MaxPooling2D 池化层 Dense 全连接层
+# Conv2D 卷积层 MaxPooling2D 池化层 Dense 全连接层
 model = models.Sequential()
-model.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(32, 32, 3))) #输出为[28,28,32]
-model.add(layers.MaxPooling2D((2, 2))) #输出为[14,14,32]
-model.add(layers.Conv2D(64, (5, 5), activation='relu')) #输出为[10,10,64]
-model.add(layers.MaxPooling2D((2, 2))) #输出为[5,5，64]
-model.add(layers.Conv2D(64, (5, 5), activation='relu')) #输出为[1,1,64]
-model.add(layers.Flatten()) #输出为[64]
-model.add(layers.Dense(32, activation='relu')) #输出为[32]
-model.add(layers.Dense(10, activation='softmax')) #输出为[10]
+model.add(layers.BatchNormalization(input_shape=(32, 32, 3), axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.MaxPooling2D((2, 2))) 
+
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.MaxPooling2D((2, 2))) 
+
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+
+model.add(layers.Flatten())
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Dense(512, activation='relu', kernel_initializer=initializers.glorot_uniform(seed=None)))
+model.add(layers.BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+model.add(layers.Dense(10, activation='softmax', kernel_initializer=initializers.glorot_uniform(seed=None)))
 model.summary()
+
+keras.utils.plot_model(model, show_shapes = True, show_layer_names = True)
 
 # 优化器 目标函数等参考 https://keras-cn.readthedocs.io/en/latest/legacy/other/optimizers/
 sgd = optimizers.SGD(lr=0.0001, decay=0.93, momentum=0.3, nesterov=False)
-adam = keras.optimizers.Adamax(lr=0.0015, beta_1=0.7, beta_2=0.998, epsilon=1e-08)
- #优化器sgd 目标函数为softmax+对数损失
+adamMax = keras.optimizers.Adamax(lr=0.0015, beta_1=0.7, beta_2=0.998, epsilon=1e-08)
+adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+m_batch_size = 32
+ #优化器adam 目标函数为softmax+对数损失
 model.compile(optimizer=adam,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=15)
+model.fit_generator(train_datagen.flow(x_train,y_train,batch_size=m_batch_size),steps_per_epoch=int(len(x_train)/m_batch_size), epochs=20)
 
-test_loss, test_acc = model.evaluate(x_test, y_test)
+test_loss, test_acc = model.evaluate_generator(test_datagen.flow(x_test, y_test))
 test_acc
